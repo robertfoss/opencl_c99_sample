a423420ae2c54288ef585e32c75b64f136f354c4
robertfoss <dev@robertfoss.se>
2012-06-12 18:10:10 +0300

    Added ocl_error() to simplify error reporting.

--- util.h
+++ util.h
@@ -1,11 +1,11 @@
-#ifndef _UTIL_H_INCLUDED
-#define _UTIL_H_INCLUDED
+#ifndef UTIL_H
+#define UTIL_H
 
 #include "CL/cl.h"
 
 char *file_contents(const char *filename, int *length);
-
 const char* ocl_error_string(cl_int error);
+void ocl_error(const char *descr, cl_int err);
 
 
-#endif
+#endif //UTIL_H_OPENSSL
